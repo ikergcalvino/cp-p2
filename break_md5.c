adfda2ccebe2186a5864afdf969cd5d0b8f6d741
Íker García Calviño <iker.gcalvino@udc.es>
2021-03-16 21:32:23 +0100

    ejercicio 2

--- break_md5.c
+++ break_md5.c
@@ -7,22 +7,22 @@
 
 #define PASS_LEN 6
 #define N_THREADS 8
+#define PROGRESS_BAR "##################################################"
 
-struct data
-{
+struct data {
     char *md5;
     int found;
-    pthread_mutex_t *mutex;
+    int attempts;
+    pthread_mutex_t *mutex_found;
+    pthread_mutex_t *mutex_attempt;
 };
 
-struct break_md5
-{
+struct break_md5 {
     int id;
     struct data *data;
 };
 
-struct thread_info
-{
+struct thread_info {
     pthread_t thread;
     struct break_md5 *args;
 };
@@ -76,9 +76,14 @@ void *break_pass(void *ptr) {
                                      // lowercase chars =>
                                     //     26 ^ PASS_LEN  different cases
     for(long i=args->id; i < bound; i += N_THREADS) {
-        pthread_mutex_lock(args->data->mutex);
+        pthread_mutex_lock(args->data->mutex_found);
         if (!args->data->found) {
-            pthread_mutex_unlock(args->data->mutex);
+            pthread_mutex_unlock(args->data->mutex_found);
+
+            pthread_mutex_lock(args->data->mutex_attempt);
+            args->data->attempts++;
+            pthread_mutex_unlock(args->data->mutex_attempt);
+
             long_to_pass(i, pass);
 
             MD5(pass, PASS_LEN, res);
@@ -86,16 +91,16 @@ void *break_pass(void *ptr) {
             to_hex(res, hex_res);
 
             if(!strcmp(hex_res, args->data->md5)) {
-                printf("%s: %s\n", args->data->md5, pass);
+                printf("\r%s: %-37s\n", args->data->md5, pass);
                 
-                pthread_mutex_lock(args->data->mutex);
+                pthread_mutex_lock(args->data->mutex_found);
                 args->data->found = 1;
-                pthread_mutex_unlock(args->data->mutex);
+                pthread_mutex_unlock(args->data->mutex_found);
 
                 break; // Found it!
             }
         } else {
-            pthread_mutex_unlock(args->data->mutex);
+            pthread_mutex_unlock(args->data->mutex_found);
             break;
         }
 
@@ -106,39 +111,76 @@ void *break_pass(void *ptr) {
     return NULL;
 }
 
-void init_data(struct data *data, char *md5) {
-    data->mutex = malloc(sizeof(pthread_mutex_t));
+void *progress(void *ptr) {
+    struct break_md5 *args = ptr;
+    double found = 0, attempts = 0, prctg;
+    long bound = ipow(26, PASS_LEN);
+    int print;
 
-    if (data->mutex == NULL) {
+    while (!found) {
+        pthread_mutex_lock(args->data->mutex_found);
+        found = (double) args->data->found;
+        pthread_mutex_unlock(args->data->mutex_found);
+
+        pthread_mutex_lock(args->data->mutex_attempt);
+        attempts = (double) args->data->attempts;
+        pthread_mutex_unlock(args->data->mutex_attempt);
+
+        prctg = 100 * (attempts / bound);
+        print = (int) (prctg * 0.5);
+
+        printf("\rSearching: [%.*s%*s] %.2f%% ", print, PROGRESS_BAR, 50 - print, "", prctg);
+        fflush(stdout);
+    }
+
+    printf(" FOUND!\n");
+
+    return NULL;
+}
+
+void init_data(struct data *data, char *md5) {
+    data->mutex_attempt = malloc(sizeof(pthread_mutex_t));
+    data->mutex_found = malloc(sizeof(pthread_mutex_t));
+
+    if (data->mutex_attempt == NULL || data->mutex_found == NULL) {
         printf("Not enough memory\n");
         exit(1);
     }
 
-    pthread_mutex_init(data->mutex, NULL);
+    pthread_mutex_init(data->mutex_attempt, NULL);
+    pthread_mutex_init(data->mutex_found, NULL);
 
     data->md5 = md5;
     data->found = 0;
+    data->attempts = 0;
 }
 
 struct thread_info *start_threads(struct data *data) {
     struct thread_info *threads;
 
-    threads = malloc(sizeof(struct thread_info) * N_THREADS);
+    threads = malloc(sizeof(struct thread_info) * (N_THREADS + 1));
 
     if (threads == NULL) {
         printf("Not enough memory\n");
         exit(1);
     }
 
-    for (long i = 0; i < N_THREADS; i++) {
+    for (long i = 0; i < N_THREADS + 1; i++) {
         threads[i].args = malloc(sizeof(struct break_md5));
 
         threads[i].args->id = i;
         threads[i].args->data = data;
 
-        if (0 != pthread_create(&threads[i].thread, NULL, break_pass, threads[i].args)) {
-            printf("Could not create thread #%d out of %d", (int) i, N_THREADS);
-            exit(1);
+        if (i < N_THREADS) {
+            if (0 != pthread_create(&threads[i].thread, NULL, break_pass, threads[i].args)) {
+                printf("Could not create thread #%d out of %d", (int) i, N_THREADS + 1);
+                exit(1);
+            }
+        } else {
+            if (0 != pthread_create(&threads[i].thread, NULL, progress, threads[i].args)) {
+                printf("Could not create thread #%d out of %d", (int) i, N_THREADS + 1);
+                exit(1);
+            }
         }
     }
 
@@ -146,14 +188,16 @@ struct thread_info *start_threads(struct data *data) {
 }
 
 void wait(struct thread_info *threads, struct data *data) {
-    for (int i = 0; i < N_THREADS; i++)
+    for (int i = 0; i < N_THREADS + 1; i++)
         pthread_join(threads[i].thread, NULL);
 
-    for (int i = 0; i < N_THREADS; i++)
+    for (int i = 0; i < N_THREADS + 1; i++)
         free(threads[i].args);
 
-    pthread_mutex_destroy(data->mutex);
-    free(data->mutex);
+    pthread_mutex_destroy(data->mutex_attempt);
+    pthread_mutex_destroy(data->mutex_found);
+    free(data->mutex_attempt);
+    free(data->mutex_found);
 
     free(threads);
 }
